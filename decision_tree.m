fd1483b1075a5c7e38a46a69560ac25403563078
Elliot-Wang <1552468439@qq.com>
2019-05-13 17:55:26 +0800

    update

--- decision_tree.m
+++ decision_tree.m
@@ -6,10 +6,12 @@ rtree = fitrtree(X,MPG);% create regression tree
 load fisheriris % load the sample data
 ctree = fitctree(meas,species); % create classification tree
 view(ctree) % text description
+
 %% 树决策
 load ionosphere % contains X and Y variables
 ctree = fitctree(X,Y);
 Ynew = predict(ctree,mean(X))
+
 %% 检测性能
 load fisheriris
 ctree = fitctree(meas,species);
